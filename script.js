9cdb85fe5a6dc1637594aa7cd1b190c9e847a075
11aghayan <11.aghayan.99@gmail.com>
2022-11-14 22:38:38 +0400

    header and nav mobile done

--- script.js
+++ script.js
@@ -2,17 +2,43 @@ const navBar = document.querySelector(".main-navigation");
 const button = document.querySelector(".menu-btn");
 const header = document.querySelector(".header-section");
 
+
+
 button.addEventListener("click", () => {
-        const buttonCondition = button.getAttribute("menu");
-        
-        if (buttonCondition === "closed") {
-            navBar.setAttribute("mobile-visibility", "true");
-            button.setAttribute("menu", "open");
-            header.setAttribute("menu", "open");
-        } else {
-            navBar.setAttribute("mobile-visibility", "false");
-            button.setAttribute("menu", "closed");
-            header.setAttribute("menu", "closed");
+            const buttonCondition = button.getAttribute("menu");
+            const navVisible = document.querySelector(".navbar");
+  
+            async function navSummon () {        
+                if (navVisible.getAttribute("navbar-visibility") === "false") {
+                    await openMenu();
+                    
+                    navVisible.setAttribute("navbar-visibility", "true");
+                } else {
+                    navVisible.setAttribute("navbar-visibility", "false");
+                }
+            }
+
+            async function openMenu () {
+            if (buttonCondition === "closed") {
+                navBar.setAttribute("mobile-visibility", "true");
+                button.setAttribute("menu", "open");
+                header.setAttribute("menu", "open");
+
+                return new Promise((resolve) => {
+                    setTimeout(() => {
+                        resolve(openMenu);
+                    }, 500);
+    
+                })
+            } else {
+                navBar.setAttribute("mobile-visibility", "false");
+                button.setAttribute("menu", "closed");
+                header.setAttribute("menu", "closed");
+            }
         }
+
+        openMenu();
+        navSummon();
     }
+    
 );
